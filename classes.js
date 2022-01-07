82f91db7210796aa7b1597a1a131ecd4802ed9ae
rbn-lara <malaru50@gmail.com>
2022-01-07 16:40:01 -0600

    initial commit

---
+++ classes.js
@@ -0,0 +1,10 @@
+class planet{
+    constructor(name, mass, radius, angle, volume, img){
+        this.name = name;
+        this.mass = mass;
+        this.radius = radius;
+        this.angle = angle;
+        this.volume = volume;
+        this.img = img;
+    }
+}
---
+++ siteInit.js
@@ -0,0 +1,80 @@
+const urlApiSolarSystem = "https://api.le-systeme-solaire.net/rest.php/bodies/";
+const planetsId = {
+    mercury: "mercure",
+    venus: "venus",
+    earth: "terre",    
+    mars: "mars",
+    jupiter: "jupiter",
+    saturn: "saturne",
+    uranus: "uranus",
+    neptune: "neptune",
+    pluto: "pluton"
+};
+
+function getPlanet(apiUrl){
+    let request = new Request(apiUrl);
+    let fetchedRequest = fetch(request).then(response => {
+        if (response.status === 200) {
+        return response.json();
+    } else {
+        throw new Error('Something went wrong on api server!');
+    }
+    }); 
+    fetchedRequest.then((content)=>{
+        let p = newPlanet(content);
+        let data = document.querySelectorAll(`.${p.name}`);
+        populateData(p, data);
+    });  
+}
+function newPlanet(json){
+    return new planet(json.englishName.toLowerCase(), json.mass, json.meanRadius, json.axialTilt, json.vol, `resources/${json.englishName.toLowerCase()}.png`);
+}
+function populateData(planet, data){
+    let imageContainer = document.getElementById(`${planet.name}`);
+    let image = document.createElement('IMG');
+    image.setAttribute('src',`${planet.img}`);
+    if(planet.name == 'saturn') image.setAttribute('style','width: 200px; height: 100px; margin-top: 50px;');
+    else image.setAttribute('style','width: 200px; height: 200px;');
+    imageContainer.appendChild(image);
+    data[0].innerHTML = `<b>Mass:</b> ${(planet.mass.massValue*10**planet.mass.massExponent).toExponential(5)} kg`;
+    data[1].innerHTML = `<b>Radius:</b> ${planet.radius} km`;
+    data[2].innerHTML = `<b>Angle:</b> ${planet.angle}°`;
+    data[3].innerHTML = `<b>Volume:</b> ${(planet.volume.volValue*10**planet.volume.volExponent).toExponential(5)} km³`;
+    data[4].innerHTML = `<b>Density:</b> ${((planet.mass.massValue*10**planet.mass.massExponent)/((planet.volume.volValue*10**planet.volume.volExponent)*1000)).toExponential(5)} kg/m³`;
+}
+function addPlanet(){
+    
+    let selectItem = document.querySelector('#selPlanet');
+    let selectedPlanet = selectItem.value;
+    let capitalized = selectedPlanet.charAt(0).toUpperCase() + selectedPlanet.slice(1);
+    
+    if(document.getElementById(`${selectedPlanet}`) != null){
+        return;
+    }
+
+    let div = document.createElement("DIV");
+    div.classList.add("card");
+    
+    let card = `<div class="card-body">
+            <h1>${capitalized}</h1>
+            <div id="${selectedPlanet}"></div>
+            <div>
+                <p class="${selectedPlanet}"></p>
+                <p class="${selectedPlanet}"></p>
+                <p class="${selectedPlanet}"></p>
+                <p class="${selectedPlanet}"></p>
+                <p class="${selectedPlanet}"></p>
+                <p class="${selectedPlanet}"></p>
+            </div>
+        </div>`;
+    div.innerHTML = card;
+
+    document.querySelector('#deckPlanets').appendChild(div);
+
+    getPlanet(urlApiSolarSystem + planetsId[selectedPlanet]);
+}
+
+getPlanet(urlApiSolarSystem + planetsId['earth']);
+
+let btn = document.querySelector('#displayPlanet');
+btn.addEventListener('click', addPlanet);
